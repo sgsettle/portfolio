68a688eadbdad6dfe4575dd596f9b75b6adbeb73
sgsettle <s.settle41@gmail.com>
2020-05-12 13:49:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// window.onscroll = () => {
+//     const nav = document.querySelector('.headerInner');
+//     if(this.scrollY <= 250){
+//          nav.className = '';
+//      } else {
+//          nav.className = 'scrolled';}
+//   };
+
+// $(window).scroll(function() {
+
+
+//     if ($(window).width() <= 750) {
+  
+//       $('nav').toggleClass('scroll', $(this).scrollTop() > 300);
+//     } else {
+//       $('nav').toggleClass('scroll', $(this).scrollTop() > 600);
+//     }
+//   });
+
+// window.scroll(function(){
+	
+// });
